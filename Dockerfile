d1e9108662030909f732f2adecb454e2e056f870
Daniel Hwang <danielleehwang@gmail.com>
2014-10-08 00:26:55 -0700

    add bower

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM node:0.10-onbuild
+FROM curious/node:onbuild
 
--- dev/run.sh
+++ dev/run.sh
@@ -8,8 +8,9 @@ GROUP=$(id -gn)
 
 [ ! -d $LOCKDIR ] && sudo mkdir -p $LOCKDIR && sudo chown -R $USER:$GROUP $LOCKDIR
 
-cd /vagrant && \
 ([ -f $CIDFILE ] && (docker stop `cat $CIDFILE` && rm -f $CIDFILE) || true) && \
+cd /vagrant && \
+(cd dev/onbuild && docker build -t curious/node:onbuild .) && \
 docker build -t curious/node . && \
 docker run -d -p 8080:8080 --cidfile $CIDFILE curious/node
 
